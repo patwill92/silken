35c17392460e0161c44758640f60a8a906486281
Pat Williams <patrick.rw92@gmail.com>
2018-05-31 20:02:18 -0400

    node server

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <Hero/>
+    <div class="row">
+      <h1>ABOUT</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+  import Hero from '@/components/home/Hero'
+
+  export default {
+    mounted() {
+      console.log(this.$style);
+    },
+    components: {
+      Hero
+    }
+  }
+</script>
